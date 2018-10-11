5bd63de946e933d97a32af015d6746bcc4fe372e
CB_ATL_124_Shante_Austin <cb_atl_124_shante_austin@PSADMINs-MacBook-Air.local>
2018-10-11 16:14:21 -0400

    Added function to click on button so that when game is not on. Do not allow game to be played

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 
+ var red = $('#red');
+ var blue = $('#blue');
+ var yellow = $('#yellow');
+ var green = $('#green');
+ var level= $('#level');
+ var on = $('#onButton')
+ var power = false;
+ var userSequence = [];
+ var sequence = [];
+ var round = 1;
+ var win;
+ var start = 'off';
+//  var lock = false;
+ var speed = 1000;
 
 
+ //When the game is turned on game will animate and flash to indicate turning on
+ // After user pressed start button. Button will light up red for the remainder of the game. 
 $('.slider').on("click", function turnOn(){
-    let red = $('#red').css("background-color", "highlightBg");
-    let blue = $('#blue')
-    let yellow = $('#yellow')
-    let green = $('#green')
     red.fadeOut(350).fadeIn(350).fadeOut(350).fadeIn(350)
     blue.delay(550).fadeOut(350).fadeIn(350).fadeOut(350).fadeIn(350)
     yellow.delay(700).fadeIn(350).fadeOut(350).fadeOut(350).fadeIn(350)
     green.delay(450).fadeIn(350).fadeOut(350).fadeOut(350).fadeIn(350)
-    let level = $('#level').css("color", "white");
-    level.fadeOut(350).fadeIn(350)
-    parseInt($)
-
-    // for(let i = 1; i >= 20; i++){
-    //     level + 1
-    // }
-
+    level.css("color", "white").fadeOut(350).fadeIn(350);
+    start = 'on'
 })
+
+
+on.click(function  (){
+    if (start === 'off'){
+     return } 
+     $('#audioOn').get(0).play();
+        on.css("background-color", "blue");
+        on.css("border-bottom" , "none");
+        on.css("border-right" , "none");
+        level.html('001');
+})
+
+
+
+
+
+
+
+// for (let level = 1; level <= 20; level++){
+//     console.log(level)
+// }
+
+
+
+
 
